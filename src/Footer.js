de880283f877dbcfa65c80a765c5d3aac7d2e955
Kesav Gopan <139054897+KesavGopan10@users.noreply.github.com>
2024-06-01 04:11:53 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -41,7 +41,7 @@ export default function Footer() {
       </MDBContainer>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
+        © 2024 Copyright:
         <a className='text-white' href='https://mdbootstrap.com/'>
           GameGalaxy
         </a>
