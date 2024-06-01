2be5b7bbf95de20756d48bfa8896be8db19beca2
Kesav Gopan <139054897+KesavGopan10@users.noreply.github.com>
2024-06-01 20:15:04 +0530

    Update Gamecard.js

--- src/Gamecard.js
+++ src/Gamecard.js
@@ -14,20 +14,20 @@ function Gamecard() {
           <MDBContainer fluid className="my-5">
             <MDBRow>
               {/* Card 1 */}
-              <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
-                <MDBCard className="glassmorphism-card">
+              <MDBCol xs="12" sm="6" lg="4" className="mb-4">
+                <MDBCard className="glassmorphism-card h-100">
                   <div className="d-flex justify-content-between p-3">
                     <p className="lead mb-0"></p>
                     <div className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style={{ width: "35px", height: "35px" }}>
                       <p className="text-white mb-0 small">FREE</p>
                     </div>
                   </div>
-                  <MDBCardImage src="assets/hangit.jpg" position="top" alt="Hangman" className="img1" />
-                  <MDBCardBody>
+                  <MDBCardImage src="assets/hangit.jpg" position="top" alt="Hangman" className="img-fluid" />
+                  <MDBCardBody className="d-flex flex-column">
                     <div className="d-flex justify-content-between mb-3">
                       <h5 className="mb-0">Hangman</h5>
                     </div>
-                    <div className="d-flex justify-content-between mb-2">
+                    <div className="d-flex justify-content-between mb-2 mt-auto">
                       <Link to="/Hangman"><button className="play">Play Now</button></Link>
                       <div className="ms-auto text-warning">
                         <MDBIcon fas icon="star" />
@@ -42,20 +42,20 @@ function Gamecard() {
               </MDBCol>
 
               {/* Card 2 */}
-              <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
-                <MDBCard className="glassmorphism-card">
+              <MDBCol xs="12" sm="6" lg="4" className="mb-4">
+                <MDBCard className="glassmorphism-card h-100">
                   <div className="d-flex justify-content-between p-3">
                     <p className="lead mb-0"></p>
                     <div className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style={{ width: "35px", height: "35px" }}>
                       <p className="text-white mb-0 small">FREE</p>
                     </div>
                   </div>
-                  <MDBCardImage src="assets/snake.jpg" position="top" alt="Snake Game" className="img1" />
-                  <MDBCardBody>
+                  <MDBCardImage src="assets/snake.jpg" position="top" alt="Snake Game" className="img-fluid" />
+                  <MDBCardBody className="d-flex flex-column">
                     <div className="d-flex justify-content-between mb-3">
                       <h5 className="mb-0">Snake Game</h5>
                     </div>
-                    <div className="d-flex justify-content-between mb-2">
+                    <div className="d-flex justify-content-between mb-2 mt-auto">
                       <Link to="/SnakeGame"><button className="play">Play Now</button></Link>
                       <div className="ms-auto text-warning">
                         <MDBIcon fas icon="star" />
@@ -70,20 +70,20 @@ function Gamecard() {
               </MDBCol>
 
               {/* Card 3 */}
-              <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
-                <MDBCard className="glassmorphism-card">
+              <MDBCol xs="12" sm="6" lg="4" className="mb-4">
+                <MDBCard className="glassmorphism-card h-100">
                   <div className="d-flex justify-content-between p-3">
                     <p className="lead mb-0"></p>
                     <div className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style={{ width: "35px", height: "35px" }}>
                       <p className="text-white mb-0 small">FREE</p>
                     </div>
                   </div>
-                  <MDBCardImage src="https://tse4.mm.bing.net/th?id=OIP.0umEsI5We9_Ipqy5akmifwHaEb&pid=Api&P=0&h=180" position="top" alt="Space Game" className="img1" />
-                  <MDBCardBody>
+                  <MDBCardImage src="https://tse4.mm.bing.net/th?id=OIP.0umEsI5We9_Ipqy5akmifwHaEb&pid=Api&P=0&h=180" position="top" alt="Space Game" className="img-fluid" />
+                  <MDBCardBody className="d-flex flex-column">
                     <div className="d-flex justify-content-between mb-3">
                       <h5 className="mb-0">Space Game</h5>
                     </div>
-                    <div className="d-flex justify-content-between mb-2">
+                    <div className="d-flex justify-content-between mb-2 mt-auto">
                       <Link to="/Spaces"><button className="play">Play Now</button></Link>
                       <div className="ms-auto text-warning">
                         <MDBIcon fas icon="star" />
@@ -101,20 +101,20 @@ function Gamecard() {
             {/* Duplicate Cards in a New Row */}
             <MDBRow>
               {/* Card 1 Duplicate */}
-              <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
-                <MDBCard className="glassmorphism-card">
+              <MDBCol xs="12" sm="6" lg="4" className="mb-4">
+                <MDBCard className="glassmorphism-card h-100">
                   <div className="d-flex justify-content-between p-3">
                     <p className="lead mb-0"></p>
                     <div className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style={{ width: "50px", height: "50px" }}>
-                      <p className="text-white mb-0 small">LOGIN </p>
+                      <p className="text-white mb-0 small">LOGIN</p>
                     </div>
                   </div>
-                  <MDBCardImage src="assets/hangit.jpg" position="top" alt="Hangman" className="img1" />
-                  <MDBCardBody>
+                  <MDBCardImage src="assets/hangit.jpg" position="top" alt="Hangman" className="img-fluid" />
+                  <MDBCardBody className="d-flex flex-column">
                     <div className="d-flex justify-content-between mb-3">
                       <h5 className="mb-0">Hangman</h5>
                     </div>
-                    <div className="d-flex justify-content-between mb-2">
+                    <div className="d-flex justify-content-between mb-2 mt-auto">
                       <Link to="/Hangman"><button className="play">Play Now</button></Link>
                       <div className="ms-auto text-warning">
                         <MDBIcon fas icon="star" />
@@ -129,20 +129,20 @@ function Gamecard() {
               </MDBCol>
 
               {/* Card 2 Duplicate */}
-              <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
-                <MDBCard className="glassmorphism-card">
+              <MDBCol xs="12" sm="6" lg="4" className="mb-4">
+                <MDBCard className="glassmorphism-card h-100">
                   <div className="d-flex justify-content-between p-3">
                     <p className="lead mb-0"></p>
                     <div className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style={{ width: "50px", height: "50px" }}>
                       <p className="text-white mb-0 small">LOGIN</p>
                     </div>
                   </div>
-                  <MDBCardImage src="assets/snake.jpg" position="top" alt="Snake Game" className="img1" />
-                  <MDBCardBody>
+                  <MDBCardImage src="assets/snake.jpg" position="top" alt="Snake Game" className="img-fluid" />
+                  <MDBCardBody className="d-flex flex-column">
                     <div className="d-flex justify-content-between mb-3">
                       <h5 className="mb-0">Snake Game</h5>
                     </div>
-                    <div className="d-flex justify-content-between mb-2">
+                    <div className="d-flex justify-content-between mb-2 mt-auto">
                       <Link to="/SnakeGame"><button className="play">Play Now</button></Link>
                       <div className="ms-auto text-warning">
                         <MDBIcon fas icon="star" />
@@ -157,20 +157,20 @@ function Gamecard() {
               </MDBCol>
 
               {/* Card 3 Duplicate */}
-              <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
-                <MDBCard className="glassmorphism-card">
+              <MDBCol xs="12" sm="6" lg="4" className="mb-4">
+                <MDBCard className="glassmorphism-card h-100">
                   <div className="d-flex justify-content-between p-3">
                     <p className="lead mb-0"></p>
                     <div className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style={{ width: "50px", height: "50px" }}>
                       <p className="text-white mb-0 small">LOGIN</p>
                     </div>
                   </div>
-                  <MDBCardImage src="https://tse4.mm.bing.net/th?id=OIP.0umEsI5We9_Ipqy5akmifwHaEb&pid=Api&P=0&h=180" position="top" alt="Space Game" className="img1" />
-                  <MDBCardBody>
+                  <MDBCardImage src="https://tse4.mm.bing.net/th?id=OIP.0umEsI5We9_Ipqy5akmifwHaEb&pid=Api&P=0&h=180" position="top" alt="Space Game" className="img-fluid" />
+                  <MDBCardBody className="d-flex flex-column">
                     <div className="d-flex justify-content-between mb-3">
                       <h5 className="mb-0">Space Game</h5>
                     </div>
-                    <div className="d-flex justify-content-between mb-2">
+                    <div className="d-flex justify-content-between mb-2 mt-auto">
                       <Link to="/Spaces"><button className="play">Play Now</button></Link>
                       <div className="ms-auto text-warning">
                         <MDBIcon fas icon="star" />
